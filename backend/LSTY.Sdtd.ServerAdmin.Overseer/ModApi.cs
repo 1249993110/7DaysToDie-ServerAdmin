010e5a5f68dfb5bee91ad05ee0dac56abf95bea3
IceCoffee <1249993110@qq.com>
2025-04-01 14:06:51 +0800

    Initial project

---
+++ backend/LSTY.Sdtd.ServerAdmin.Overseer/ModApi.cs
@@ -0,0 +1,10 @@
+﻿namespace LSTY.Sdtd.ServerAdmin.Overseer
+{
+    public class ModApi : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ backend/LSTY.Sdtd.ServerAdmin.WebApi/Program.cs
@@ -0,0 +1,25 @@
+namespace LSTY.Sdtd.ServerAdmin.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
