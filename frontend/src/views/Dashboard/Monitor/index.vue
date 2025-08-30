23c36fb7480adb6a062f103b71e3c7eef6c3f476
IceCoffee <1249993110@qq.com>
2025-08-31 00:42:40 +0800

    Enhance network monitoring chart with dynamic Y-axis options and data formatting

--- frontend/src/views/Dashboard/Monitor/index.vue
+++ frontend/src/views/Dashboard/Monitor/index.vue
@@ -20,13 +20,87 @@ const props = defineProps({
 
 const { t } = useI18n();
 const appStore = useAppStore();
-const unit = ref('');
 
+const chartData = ref();
+const chartOptions = ref();
+
+const MAX_DATA_POINTS = 8;
+const UNITS = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb']; // Define the base and units
+
+const formatBytesToSpeed = (bits, precision = 1) => {
+    if (bits === 0) return '0 b';
+    const k = 1024;
+
+    const i = Math.floor(Math.log(bits) / Math.log(k));
+    const finalUnitIndex = i < 0 ? 0 : i;
+    const value = bits / Math.pow(k, finalUnitIndex);
+
+    return `${parseFloat(value.toFixed(precision))} ${UNITS[finalUnitIndex]}`;
+};
+
+const getDynamicYAxisOptions = (maxBits) => {
+    if (maxBits === 0) {
+        return { unit: 'b', stepSize: 1 };
+    }
+
+    const k = 1024;
+    const presets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+    const maxPreset = presets[presets.length - 1];
+
+    let unitIndex = 0;
+    let value = maxBits;
+    // When the value is greater than the maximum preset value, the unit is upgraded.
+    while (value > maxPreset && unitIndex < UNITS.length - 1) {
+        value /= k;
+        unitIndex++;
+    }
+    // Find the first preset value that is greater than or equal to the current value
+    let ceiledValue = presets.find((p) => p >= value);
+
+    // If not found (i.e. value is still greater than the maximum preset value, but is already the last unit), round up to the nearest multiple of 100
+    if (ceiledValue === undefined) {
+        ceiledValue = Math.ceil(value / 100) * 100;
+    }
+
+    const max = ceiledValue * Math.pow(k, unitIndex);
+    let stepSize;
+    if (ceiledValue % 3 === 0) {
+        stepSize = max / 3;
+    } else if (ceiledValue % 4 === 0) {
+        stepSize = max / 4;
+    } else {
+        stepSize = max / 2;
+    }
+
+    return {
+        unit: UNITS[unitIndex],
+        stepSize: stepSize,
+        ceiledValue: ceiledValue,
+    };
+};
+
+let lastMax = {
+    value: 0,
+    config: {},
+};
 const getChartOptions = () => {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--p-text-color');
     const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
     const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+    const maxBytes = Math.max(...chartData.value.datasets.map((dataset) => Math.max(...dataset.data)));
+    let yAxisConfig;
+    if (maxBytes !== 0 && maxBytes === lastMax.value) {
+        yAxisConfig = lastMax.config;
+    } else {
+        yAxisConfig = getDynamicYAxisOptions(maxBytes);
+        lastMax = {
+            value: maxBytes,
+            config: yAxisConfig,
+        };
+    }
+    console.log(yAxisConfig);
 
     return {
         responsive: true,
@@ -40,7 +114,7 @@ const getChartOptions = () => {
                 rtl: appStore.isRTL,
                 callbacks: {
                     label: (context) => {
-                        return `${context.parsed.y} ${unit.value}`;
+                        return formatBytesToSpeed(context.parsed.y) + 'ps';
                     },
                 },
             },
@@ -68,8 +142,9 @@ const getChartOptions = () => {
             y: {
                 ticks: {
                     color: textColorSecondary,
+                    stepSize: yAxisConfig.stepSize,
                     callback: (value) => {
-                        return value + ' Kb';
+                        return formatBytesToSpeed(value);
                     },
                 },
                 grid: {
@@ -77,7 +152,7 @@ const getChartOptions = () => {
                 },
                 title: {
                     display: true,
-                    text: t('views.dashboard.monitor.network'),
+                    text: `${t('views.dashboard.monitor.network')} (${yAxisConfig.unit}ps / s)`,
                 },
                 min: 0,
             },
@@ -85,47 +160,65 @@ const getChartOptions = () => {
     };
 };
 
-const chartData = ref();
-const chartOptions = ref();
+const getChartData = (newDate, newData) => {
+    var chartDataVal = chartData.value;
+    const result = {
+        datasets: [
+            {
+                label: t('views.dashboard.monitor.trafficIn'),
+                fill: true,
+                tension: 0.4,
+                borderColor: '#1F77B4',
+                backgroundColor: 'rgba(107, 114, 128, 0.2)',
+            },
+            {
+                label: t('views.dashboard.monitor.trafficOut'),
+                fill: true,
+                borderColor: '#2CA02C',
+                tension: 0.4,
+                backgroundColor: 'rgba(107, 114, 128, 0.2)',
+            },
+        ],
+    };
+
+    if (!chartDataVal) {
+        const now = dayjs();
+        result.labels = Array.from({ length: MAX_DATA_POINTS }, (_, index) => now.subtract((MAX_DATA_POINTS - index - 1) * 3, 'seconds').format('HH:mm:ss'));
+        for (let i = 0, len = result.datasets.length; i < len; i++) {
+            result.datasets[i].data = Array.from({ length: MAX_DATA_POINTS }, () => 0);
+        }
+    } else {
+        if (!newDate || !newData) {
+            result.labels = chartDataVal.labels;
+            for (let i = 0, len = result.datasets.length; i < len; i++) {
+                result.datasets[i].data = chartDataVal.datasets[i].data;
+            }
+        } else {
+            result.labels = chartDataVal.labels.slice(1).concat(newDate.format('HH:mm:ss'));
+            for (let i = 0, len = chartDataVal.datasets.length; i < len; i++) {
+                result.datasets[i].data = chartDataVal.datasets[i].data.slice(1).concat(newData[i]);
+            }
+        }
+    }
+
+    return result;
+};
 
 onMounted(() => {
+    chartData.value = getChartData();
     chartOptions.value = getChartOptions();
 });
 
 watch(
     () => [props.timestamp, props.networkInfos],
     (newVal, oldVal) => {
-        const lastDate = dayjs(oldVal[0]);
-        const length = 8;
-
         if (!oldVal[0]) {
-            const labels = Array.from({ length: length }, (_, index) => lastDate.subtract((length - index - 1) * 3, 'seconds').format('HH:mm:ss'));
-            chartData.value = {
-                labels: labels,
-                datasets: [
-                    {
-                        label: t('views.dashboard.monitor.trafficIn'),
-                        data: Array.from({ length: length }, () => 0),
-                        fill: true,
-                        tension: 0.4,
-                        borderColor: '#1F77B4',
-                        backgroundColor: 'rgba(107, 114, 128, 0.2)',
-                    },
-                    {
-                        label: t('views.dashboard.monitor.trafficOut'),
-                        data: Array.from({ length: length }, () => 0),
-                        fill: true,
-                        borderColor: '#2CA02C',
-                        tension: 0.4,
-                        backgroundColor: 'rgba(107, 114, 128, 0.2)',
-                    },
-                ],
-            };
             return;
         }
 
-        const currentDate = dayjs(newVal[0]);
-        const timeDifferenceInSeconds = currentDate.diff(lastDate, 'millisecond');
+        const oldDate = dayjs(oldVal[0]);
+        const newDate = dayjs(newVal[0]);
+        const timeDifferenceInSeconds = newDate.diff(oldDate, 'millisecond') / 1000;
 
         const newBytesReceived = newVal[1].map((i) => i.bytesReceived).reduce((a, b) => a + b, 0);
         const oldBytesReceived = oldVal[1].map((i) => i.bytesReceived).reduce((a, b) => a + b, 0);
@@ -133,24 +226,17 @@ watch(
         const oldBytesSent = oldVal[1].map((i) => i.bytesSent).reduce((a, b) => a + b, 0);
 
         const downloadSpeedInBytes = newBytesReceived - oldBytesReceived;
-        // 将字节转换为兆比特（Mb）
-        const downloadSpeedInMbps = (downloadSpeedInBytes * 8) / ((1024 * timeDifferenceInSeconds) / 1000);
-
-        // 计算上行（上传）网速
         const uploadSpeedInBytes = newBytesSent - oldBytesSent;
-        // 将字节转换为兆比特（Mb）
-        const uploadSpeedInMbps = (uploadSpeedInBytes * 8) / ((1024 * timeDifferenceInSeconds) / 1000);
 
-        var chartDataVal = chartData.value;
-        chartDataVal.labels = chartDataVal.labels.slice(1).concat(currentDate.format('HH:mm:ss'));
-        chartDataVal.datasets[0].data = chartDataVal.datasets[0].data.slice(1).concat(downloadSpeedInMbps.toFixed(2));
-        chartDataVal.datasets[1].data = chartDataVal.datasets[1].data.slice(1).concat(uploadSpeedInMbps.toFixed(2));
+        chartData.value = getChartData(newDate, [(downloadSpeedInBytes / timeDifferenceInSeconds) * 8, (uploadSpeedInBytes / timeDifferenceInSeconds) * 8]);
+        chartOptions.value = getChartOptions();
     }
 );
 
 const localeStore = useLocaleStore();
 watch([() => appStore.primaryColor, () => appStore.isRTL, () => appStore.isDark, () => localeStore.lang], async () => {
     await nextTick();
+    chartData.value = getChartData();
     chartOptions.value = getChartOptions();
 });
 </script>
